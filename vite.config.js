ec96c1cd4e9a67d4c51d117236906450d7b7ed91
“ram” <ramuaryal1987@gmail.com>
2025-06-28 12:13:33 +1200

    Removed base path for Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/protfoliolatest/", // <- Important for GitHub Pages
+  base: "/", // ✅ Good for Vercel
 });
